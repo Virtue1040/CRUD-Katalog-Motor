8d5ff22d78031daf2a04ac6acdf5893f10ff1d21
rafi.hidayat1313 <rafi.hidayat1313@gmail.com>
2025-06-13 15:05:06 +0000

    tambah seeder

--- database/seeders/KatalogSeeder.php
+++ database/seeders/KatalogSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Katalog;
 
 class KatalogSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class KatalogSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Katalog::create([
+            'judul' => 'Motor Beat',
+            'manufacturer' => 'Honda',
+            'harga' => 10000000.0,
+            'imageUrl' => 'upload/motor-beat.jpg'
+        ]);
+
+        Katalog::create([
+            'judul' => 'Motor Xmax',
+            'manufacturer' => 'Yamaha',
+            'harga' => 10000000.0,
+            'imageUrl' => 'upload/motor-xmax.jpg'
+        ]);
     }
 }
